9993794cea13e9775f7b9b90250170ff6444994e
Sandsten <staffansandberg@outlook.com>
2021-08-29 16:31:17 +0200

    Some tutorials

--- src/Incrementor.elm
+++ src/Incrementor.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module Incrementor exposing (..)
 
 -- Press buttons to increment and decrement a counter.
 --
@@ -15,50 +15,45 @@ import Html.Events exposing (onClick)
 
 -- MAIN
 
-
+-- main value describes what gets shown on the screen
+-- view will show everything on the screen
+-- user input will be fed to the update function
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-
-
 -- MODEL
-
-
 type alias Model = Int
-
-
 init : Model
 init =
   0
 
-
-
 -- UPDATE
 
-
 type Msg
-  = Increment
+  = Increment Int
   | Decrement
-
+  | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    Increment ammount->
+      model + ammount
 
     Decrement ->
       model - 1
 
-
+    Reset ->
+      0
 
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick (Increment 1)] [ text "+" ]
+    , button [ onClick (Increment 10)] [ text "+10" ]
+    , button [ onClick Reset ] [ text "Reset" ]
     ]

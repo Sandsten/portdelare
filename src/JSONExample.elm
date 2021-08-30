2d375ab6e18a035ea72b0dfcc2d19fa4350bf3fd
Sandsten <staffansandberg@outlook.com>
2021-08-30 15:51:21 +0200

    JSON decoding example

--- src/JSONExample.elm
+++ src/JSONExample.elm
@@ -24,7 +24,10 @@ main =
 
 -- MODEL
 
-
+{--
+  We defines this app to be able to have three different states
+  If it's successfull it will have a string to store! Which is the url to our cat gif
+--}
 type Model
   = Failure
   | Loading
@@ -39,7 +42,14 @@ init _ =
 
 -- UPDATE
 
+{--
+  Two types of Messages which can be passed to our elm code
+  Either the user wants a new cat gif
+  or a cat gif has been recieved from the api call
+  (Result Http.Error String) is standard where the last element is the type we want the result to be in the end
 
+  If you call these types as functions from view, they will be handled in the update function accordingly
+--}
 type Msg
   = MorePlease
   | GotGif (Result Http.Error String)
@@ -95,6 +105,11 @@ viewGif model =
 
     Success url ->
       div []
+        {--
+          MorePlease is connected to our update type above.
+          I think that what's returned as Msg which the update function will handle.
+          !!! Kinda confusing that MorePlease called as a function? I don't fully understand that yet
+        --}
         [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
         , img [ src url ] []
         ]
@@ -111,7 +126,8 @@ getRandomCatGif =
     , expect = Http.expectJson GotGif gifDecoder
     }
 
-
-gifDecoder : Decoder String
+-- Many different decoder exist depending on what type of data we are handling
+-- Package for handling multiple fields more easily NoRedInk/elm-json-decode-pipeline
+gifDecoder : Decoder String -- Decoder and type we want to retun
 gifDecoder =
-  field "data" (field "image_url" string)
+  field "data" (field "image_url" string) -- Specify which field we want to extract

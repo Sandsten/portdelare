c6fa420306c461f474ab2483ec24451e548154ec
Sandsten <staffansandberg@outlook.com>
2021-08-31 23:09:37 +0200

    Analogue clock example base

--- src/ClockExample.elm
+++ src/ClockExample.elm
@@ -7,7 +7,10 @@ import Time
 import Json.Decode exposing (bool)
 import Debug exposing (log)
 import Html.Events exposing (onClick)
-
+import Svg exposing(..)
+import Svg.Attributes exposing(..)
+import String exposing (toInt)
+import Html.Attributes exposing (style)
 
 
 -- MAIN
@@ -87,12 +90,137 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    hour   = String.fromInt (Time.toHour   model.zone model.time)
-    minute = String.fromInt (Time.toMinute model.zone model.time)
-    second = String.fromInt (Time.toSecond model.zone model.time)
+    hour   = Time.toHour   model.zone model.time
+    minute = Time.toMinute model.zone model.time
+    second = Time.toSecond model.zone model.time
   in
   div []
-  [
-    h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
-  , button [onClick TogglePause] [text "Pause"]
+  [ 
+    svg 
+    [ width "100vh"
+    , height "100vh"
+    , viewBox "0 0 500 500"
+    , Svg.Attributes.style "margin-right: auto; margin-left: auto; display: block"
+    ]
+    [ circle
+      [ cx "250"
+      , cy "250"
+      , r  "240"
+      , fill "skyblue"
+      ]
+      []
+    , viewHourHand hour minute
+    , viewMinuteHand minute
+    , viewSecondHand second
+      -- How do we loop this???
+    , viewHour 1
+    , viewHour 2
+    , viewHour 3
+    , viewHour 4
+    , viewHour 5
+    , viewHour 6
+    , viewHour 7
+    , viewHour 8
+    , viewHour 9
+    , viewHour 10
+    , viewHour 11
+    , viewHour 12
+    , circle
+      [ cx "250"
+      , cy "250"
+      , r  "5"
+      , fill "black"
+      ]
+      []
+    ]
   ]
+
+viewHour : Int -> Svg Msg
+viewHour hour = 
+  let
+    rotation = toFloat hour * (360/12) - 90
+    r1 = 190
+    r2 = 235
+    origin = 250
+    p1x = origin + (cos (degrees rotation) * r1)
+    p1y = origin + (sin (degrees rotation) * r1)
+    p2x = origin + (cos (degrees rotation) * r2)
+    p2y = origin + (sin (degrees rotation) * r2)
+  in
+  line
+  [ x1 (String.fromFloat p1x)
+  , y1 (String.fromFloat p1y)
+  , x2 (String.fromFloat p2x)
+  , y2 (String.fromFloat p2y)
+  , stroke "black"
+  , strokeWidth "10"
+  ]
+  [] 
+
+viewSecondHand : Int -> Svg Msg
+viewSecondHand second =
+  let
+    rotation = toFloat second * (360/60) - 90
+    r1 = 0
+    r2 = 180
+    origin = 250
+    p1x = origin + (cos (degrees rotation) * r1)
+    p1y = origin + (sin (degrees rotation) * r1)
+    p2x = origin + (cos (degrees rotation) * r2)
+    p2y = origin + (sin (degrees rotation) * r2)
+  in
+  line
+  [ x1 (String.fromFloat p1x)
+  , y1 (String.fromFloat p1y)
+  , x2 (String.fromFloat p2x)
+  , y2 (String.fromFloat p2y)
+  , stroke "black"
+  , strokeWidth "2"
+  ]
+  [] 
+
+viewMinuteHand : Int -> Svg Msg
+viewMinuteHand minute =
+  let
+    rotation = toFloat minute * (360/60) - 90
+    r1 = 0
+    r2 = 180
+    origin = 250
+    p1x = origin + (cos (degrees rotation) * r1)
+    p1y = origin + (sin (degrees rotation) * r1)
+    p2x = origin + (cos (degrees rotation) * r2)
+    p2y = origin + (sin (degrees rotation) * r2)
+  in
+  line
+  [ x1 (String.fromFloat p1x)
+  , y1 (String.fromFloat p1y)
+  , x2 (String.fromFloat p2x)
+  , y2 (String.fromFloat p2y)
+  , stroke "black"
+  , strokeWidth "5"
+  ]
+  [] 
+
+viewHourHand : Int -> Int -> Svg Msg
+viewHourHand hour minute =
+  let
+    hourAnalogue = (modBy 12 hour)
+    extraRotation = (toFloat minute / 60) * (360/12) -- Hour hand moves slowly between hours based on minutes
+    rotation = toFloat hourAnalogue * (360/12) - 90 + extraRotation
+    r1 = 0
+    r2 = 90
+    origin = 250
+    p1x = origin + (cos (degrees rotation) * r1)
+    p1y = origin + (sin (degrees rotation) * r1)
+    p2x = origin + (cos (degrees rotation) * r2)
+    p2y = origin + (sin (degrees rotation) * r2)
+  in
+  line
+  [ x1 (String.fromFloat p1x)
+  , y1 (String.fromFloat p1y)
+  , x2 (String.fromFloat p2x)
+  , y2 (String.fromFloat p2y)
+  , stroke "black"
+  , strokeWidth "5"
+  ]
+  [] 

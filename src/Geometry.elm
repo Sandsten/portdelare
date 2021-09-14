4b70c29fe8a416a31b879401e5a73f33e2d20e1f
Emil Jacobsen <emiloddjacobsen@gmail.com>
2021-09-14 11:45:17 +0200

    Improved physics (still a bit of a fulhack)

--- src/Geometry.elm
+++ src/Geometry.elm
@@ -10,6 +10,11 @@ scale : Float -> Vector -> Vector
 scale a v =
   Vector (a * v.x) (a * v.y)
 
+rescale : Float -> Vector -> Vector
+rescale a v =
+  if isNullVector v then v
+  else scale (a / (norm v)) v
+
 flip : Vector -> Vector
 flip v = scale (-1) v
 
@@ -22,6 +27,9 @@ normalise v =
     size = norm v
   in
     if size == 0 then v else scale (1 / size) v
+
+nullVector : Vector
+nullVector = { x = 0, y = 0 }
 
 isNullVector : Vector -> Bool
 isNullVector v = (v.x == 0) && (v.y == 0)
--- src/Player.elm
+++ src/Player.elm
@@ -25,7 +25,7 @@ defaultAcceleration =
 
 defaultDrag : Float
 defaultDrag =
-    0.018
+    0.025
 
 
 initial : Player
@@ -40,10 +40,9 @@ initial =
 animate : Float -> Vector -> Player -> Player
 animate dt direction p =
   let
-    newVelocity = add p.velocity (scale dt p.acceleration)
+    newVelocity = computeVelocity dt (not <| isNullVector direction) p.acceleration p.velocity
     newSpeed = norm newVelocity
     newPos = add p.pos (scale dt p.velocity)
-    
   in
     { p
     | pos = Vector
@@ -51,12 +50,26 @@ animate dt direction p =
         if newPos.x >= 4 - 1/4 then 4 - 1/4 else newPos.x
       )
       ( newPos.y )
-    , velocity = if (newSpeed <= maxSpeed)
-                 then newVelocity
-                 else scale (maxSpeed/newSpeed) newVelocity
+    , velocity = newVelocity
     , acceleration =
       if isNullVector direction then
         scale (newSpeed * defaultDrag) <| flip <| normalise p.velocity
       else
         scale defaultAcceleration direction
     }
+
+-- First, we bound the speed by maxSpeed.
+-- Then, we set the velocity to zero if:
+--  * there is no force acting on the object, and
+--  * the speed change is larger than the speed.
+computeVelocity dt hasForce acceleration velocity = 
+  let
+    naiveVelocity = add velocity (scale dt acceleration)
+    naiveSpeed = norm naiveVelocity
+    cappedSpeed = min naiveSpeed maxSpeed
+    cappedVelocity = rescale cappedSpeed naiveVelocity
+
+  in
+    if (hasForce || (dt * (norm acceleration) >= (norm velocity)))
+    then cappedVelocity
+    else nullVector
